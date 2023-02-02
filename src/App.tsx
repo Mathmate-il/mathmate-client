b103985e2c121d054132ed451896c47ecebab88f
Amit Bar-Gil <amit217@yandex.com>
2023-02-02 21:33:23 +0200

    refactor: restart settings

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,34 @@
-import { Route, Routes } from 'react-router-dom';
-import './App.css';
-import Navbar from './components/navbar';
-import Home from './pages/HomePage';
-import NewQuestion from './pages/NewQuestionPage';
-import NotFound from './pages/NotFoundPage';
-import Profile from './pages/ProfilePage';
-import SearchResult from './pages/SearchResultPage';
-import ErrorBoundary from './components/errorBoundary';
-import GoogleAuth from './components/googleAuth';
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
 
 function App() {
+  const [count, setCount] = useState(0)
+
   return (
-    <ErrorBoundary>
-      <GoogleAuth />
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/searchResult" element={<SearchResult />} />
-        <Route path="/newQuestion" element={<NewQuestion />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </ErrorBoundary>
-  );
+    <div className="App">
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src="/vite.svg" className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://reactjs.org" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </div>
+  )
 }
 
-export default App;
+export default App
--- src/typescript/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './entities/question';
-export * from './entities/answer';
-export * from './entities/user';
-export * from './entities/tag';
