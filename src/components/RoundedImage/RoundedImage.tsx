5103292c59cca682988d6d0c58dc0bdaf9bbec2c
Erez Birenholz <biran330@gmail.com>
2023-02-12 04:31:34 +0200

    add storybook

--- src/components/RoundedImage/RoundedImage.tsx
+++ src/components/RoundedImage/RoundedImage.tsx
@@ -1,10 +1,11 @@
-interface RoundedImageProps {
+export interface RoundedImageProps {
   src: string;
   alt: string;
   width: string | number;
   height: string | number;
 }
 
+// eslint-disable-next-line object-curly-newline
 function RoundedImage({ src, alt, width, height }: RoundedImageProps) {
   return (
     <img
