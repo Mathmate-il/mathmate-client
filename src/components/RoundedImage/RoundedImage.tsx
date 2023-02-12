cb19ccbd37caf578fe8209fe4ccb969ed65e144c
Erez Birenholz <biran330@gmail.com>
2023-02-12 03:42:35 +0200

    add rounded image and tab control

---
+++ src/components/RoundedImage/RoundedImage.tsx
@@ -0,0 +1,22 @@
+interface RoundedImageProps {
+  src: string;
+  alt: string;
+  width: string | number;
+  height: string | number;
+}
+
+function RoundedImage({ src, alt, width, height }: RoundedImageProps) {
+  return (
+    <img
+      src={src}
+      alt={alt}
+      style={{
+        borderRadius: '50%',
+        width,
+        height,
+      }}
+    />
+  );
+}
+
+export default RoundedImage;
