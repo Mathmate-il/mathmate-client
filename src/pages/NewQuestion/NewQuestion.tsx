e667a4e41f38e9b008a9b5c555d1c2f0df9abd18
itaim18 <37772742+itaim18@users.noreply.github.com>
2023-02-15 00:26:14 +0200

    adding new Question screen

--- src/pages/NewQuestion/NewQuestion.tsx
+++ src/pages/NewQuestion/NewQuestion.tsx
@@ -1,5 +1,75 @@
+import React from 'react';
 import './NewQuestion.scss';
-
+import Footer from '../../components/Footer';
+import { boolean } from 'zod';
+const initialTags = {
+  Math: false,
+  Algebra: false,
+  Geometry: false,
+};
 export default function NewQuestion() {
-  return <></>;
+  const [tags, setTags] = React.useState(initialTags);
+  const [text, setText] = React.useState('');
+  const [title, setTitle] = React.useState('');
+  const tagsList = Object.keys(initialTags);
+  const handleSubmit = (e: React.SyntheticEvent) => {
+    e.preventDefault();
+    console.log({ title: title, text: text, tags: tags });
+  };
+  return (
+    <div className="home">
+      <header className="header">
+        <h1 className="heading-1">MathMate</h1>
+      </header>
+      <div className="contentBody newQ">
+        <form className="form" onSubmit={handleSubmit}>
+          <h1 className="heading-1" style={{ fontSize: '48px' }}>
+            New Question
+          </h1>
+          <label htmlFor="title">Title:</label>
+          <input
+            type="text"
+            name="title"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
+          <label htmlFor="tag">Tags:</label>
+          <fieldset className="tags">
+            {tagsList.map((option) => (
+              <div key={option} className="tags" style={{ padding: '5px' }}>
+                <input
+                  type="checkbox"
+                  id={option}
+                  value={option}
+                  checked={tags[option as keyof typeof boolean]}
+                  onChange={(event) => {
+                    setTags({
+                      ...tags,
+                      [option]: event.target.checked,
+                    });
+                  }}
+                />
+                <label className="tag" htmlFor={option}>
+                  {option}
+                </label>
+              </div>
+            ))}
+          </fieldset>
+          <label htmlFor="text">Text:</label>
+          <textarea
+            name="text"
+            cols={42}
+            rows={16}
+            className="qText"
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+          ></textarea>
+          <button className="btn" style={{ width: 'auto' }}>
+            Ask
+          </button>
+        </form>
+      </div>
+      <Footer />
+    </div>
+  );
 }
